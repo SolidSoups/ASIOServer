e8e592a19a532cc5f632972c03fb600f364761ee
SolidSoups <eli.marc.brown@gmail.com>
2025-04-21 16:02:19 +0200

    finished the net_server

--- NetClient/SimpleClient.cpp
+++ NetClient/SimpleClient.cpp
@@ -1,33 +1,24 @@
 #include <cstdio>
 #include <iostream>
 
+#include "net_client.h"
 #include "olc_net.h"
 
 enum class CustomMsgTypes : uint32_t { FireBullet, MovePlayer };
 
+class CustomClient : public olc::net::client_interface<CustomMsgTypes> {
+public:
+  bool FireBullet(float x, float y) {
+    olc::net::message<CustomMsgTypes> msg;
+    msg.header.id = CustomMsgTypes::FireBullet;
+    msg << x << y;
+    Send(msg);
+  }
+};
+
 int main() {
-  olc::net::message<CustomMsgTypes> msg;
-  msg.header.id = CustomMsgTypes::FireBullet;
-
-  int a = 1;
-  bool b = true;
-  float c = 3.14159;
-  struct {
-    float x;
-    float y;
-  } d[5];
-
-  msg << a << b << c << d;
-
-  a = 21;
-  b = false;
-  c = 99.0f;
-
-  msg >> d >> c >> b >> a;
-  std::cout << "a: " << a << '\n';
-  std::cout << "b: " << b << '\n';
-  std::cout << "c: " << c << '\n';
-  std::cout << "d: " << d << '\n';
-
+  CustomClient c;
+  c.Connect("community.onelonecoder.com", 60000);
+  c.FireBullet(2.0f, 5.0f);
   return 0;
 }
