1dc4fc88b51a47063fbd6eb8da403f45037b678b
SolidSoups <eli.marc.brown@gmail.com>
2025-04-23 00:53:44 +0200

    deleted some cpy files. squashed a bug. Apperantly, when creating a net_message, the size of the header was being set incorrectly. the size was supposed to reflect the size of the body, but in the tutorial, it was set to the size of the head + the size of the body. Which means when i was sending a ping to the server, it was expecting 16 bytes (8bytes for the header and 8bytes for the body) but only 8 bytes were ever being sent.

--- NetClient/SimpleClient.cpp
+++ NetClient/SimpleClient.cpp
@@ -1,10 +1,12 @@
 
+#include <conio.h>
+
 #include <iostream>
 
 #include "olc_net.h"
-#include <conio.h>
 
-enum class CustomMsgTypes : uint32_t {
+enum class CustomMsgTypes : uint32_t
+{
   ServerAccept,
   ServerDeny,
   ServerPing,
@@ -12,23 +14,25 @@ enum class CustomMsgTypes : uint32_t {
   ServerMessage,
 };
 
-class CustomClient : public olc::net::client_interface<CustomMsgTypes> {
+class CustomClient : public olc::net::client_interface<CustomMsgTypes>
+{
 public:
-  void PingServer() {
+  void PingServer()
+  {
     std::cout << "[CLIENT] Pinging server...\n";
     olc::net::message<CustomMsgTypes> msg;
     msg.header.id = CustomMsgTypes::ServerPing;
 
     // Caution with this
-    std::chrono::system_clock::time_point timeNow =
-        std::chrono::system_clock::now();
+    std::chrono::system_clock::time_point timeNow = std::chrono::system_clock::now();
 
     msg << timeNow;
     Send(msg);
   }
 };
 
-int main() {
+int main()
+{
   CustomClient c;
   c.Connect("127.0.0.1", 60000);
 
@@ -36,9 +40,10 @@ int main() {
   bool old_key[3] = {false, false, false};
 
   bool bQuit = false;
-  while (!bQuit) {
-
-    if (_kbhit()) {
+  while (!bQuit)
+  {
+    if (_kbhit())
+    {
       int ch = _getch();
       if (ch == '1')
         c.PingServer();
@@ -46,23 +51,28 @@ int main() {
         bQuit = true;
     }
 
-    if (c.IsConnected()) {
-      if (!c.Incoming().empty()) {
+    if (c.IsConnected())
+    {
+      if (!c.Incoming().empty())
+      {
         auto msg = c.Incoming().pop_front().msg;
 
-        switch (msg.header.id) {
-        case CustomMsgTypes::ServerPing: {
-          std::chrono::system_clock::time_point timeNow =
-              std::chrono::system_clock::now();
-          std::chrono::system_clock::time_point timeThen;
-          msg >> timeThen;
-          std::cout << "Ping: "
-                    << std::chrono::duration<double>(timeNow - timeThen).count()
-                    << "\n";
-        } break;
+        switch (msg.header.id)
+        {
+          case CustomMsgTypes::ServerPing:
+          {
+            std::chrono::system_clock::time_point timeNow = std::chrono::system_clock::now();
+            std::chrono::system_clock::time_point timeThen;
+            msg >> timeThen;
+            std::cout << "Ping: " << std::chrono::duration<double>(timeNow - timeThen).count()
+                      << "\n";
+          }
+          break;
         }
       }
-    } else {
+    }
+    else
+    {
       std::cout << "Server Down\n";
       bQuit = true;
     }
